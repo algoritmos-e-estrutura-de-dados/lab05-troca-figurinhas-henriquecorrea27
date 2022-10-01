5078fbb9e92c972904d0af17ad7d562250167795
henriquecorrea27 <henrique.correa@edu.unifil.br>
2022-09-30 23:19:26 -0300

    figurinhas feito

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,34 +1,35 @@
-def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao, A, B):
+def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
     figMaria = 0
     figJoao = 0
-    i = 0
-    j = 0
-    k = 0
-    l = 0
+
+    figurinhas_da_maria = list(set(figurinhas_da_maria))
+    figurinhas_do_joao = list(set(figurinhas_do_joao))
 
 
-    for i in range(int(A)):
-        for j in range(int(B)):
+    for i in range(len(figurinhas_da_maria)):
+        for j in range(len(figurinhas_do_joao)):
             if figurinhas_da_maria[i] == figurinhas_do_joao[j]:
-                figMaria += 1           
-    
+                figMaria += 1  
 
-    for k in range(int(B)):
-        for l in range(int(A)):
+
+    for k in range(len(figurinhas_do_joao)):
+        for l in range(len(figurinhas_da_maria)):
             if figurinhas_do_joao[k] == figurinhas_da_maria[l]:
                 figJoao += 1
 
 
-    if (int(A) - int(figMaria)) < (int(B) - int(figJoao)):
-        return print(f"Máximo de figurinhas para troca {int(A) - int(figMaria)}")
+
+
+    if (len(figurinhas_da_maria) - int(figMaria)) < (len(figurinhas_do_joao) - int(figJoao)):
+        return print(f"Máximo de figurinhas para troca {len(figurinhas_da_maria) - int(figMaria)}")
     else:
-        return print(f"Máximo de figurinhas para troca {int(B) - int(figJoao)}")
+        return print(f"Máximo de figurinhas para troca {len(figurinhas_do_joao) - int(figJoao)}")
         
 
 if __name__ == '__main__':
     A, B = input().split(' ')
     figurinhas_da_maria = input().split(' ')
     figurinhas_da_joao = input().split(' ')
-    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao, A, B)
+    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
 
 
